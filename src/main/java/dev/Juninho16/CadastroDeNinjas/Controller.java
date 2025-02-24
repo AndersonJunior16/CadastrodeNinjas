f68a5172d4a5c9979eeec7a26c80ff5532513f50
Anderson Junior <136769269+AndersonJunior16@users.noreply.github.com>
2025-02-24 16:39:32 -0300

    Criação de entidade no banco de dados atraves do NinjaModel

--- src/main/java/dev/Juninho16/CadastroDeNinjas/Controller.java
+++ src/main/java/dev/Juninho16/CadastroDeNinjas/Controller.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class Controller {
 
-    @GetMapping("boasVindas")
+    @GetMapping("/boasVindas")
     public String boasVindas(){
         return "Essa é minha primeira mensagem";
     }
