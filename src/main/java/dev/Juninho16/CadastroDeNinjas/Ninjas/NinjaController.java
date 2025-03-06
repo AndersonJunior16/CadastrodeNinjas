760608fd7bcf12a101a2b87c0192138ee990cafa
Anderson Junior <136769269+AndersonJunior16@users.noreply.github.com>
2025-03-06 14:13:52 -0300

    feat: Adiciona um CRUD no NinjaController

--- src/main/java/dev/Juninho16/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/Juninho16/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,8 +1,6 @@
 package dev.Juninho16.CadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -11,6 +9,37 @@ public class NinjaController {
     @GetMapping("/boasVindas")
     public String boasVindas(){
         return "Essa é minha primeira mensagem";
+    }
+
+
+    //Adicionar ninjas (CREATE)
+    @PostMapping("/adicionar")
+    public String criarNinja(){
+        return "Ninja Criado";
+    }
+
+    //Prucurar Ninja ID (READ)
+    @GetMapping("/ninjaid")
+    public String mostrarNinjaId(){
+        return "Mostrar ninja por ID";
+    }
+
+    //Mostrar todos os ninjas (READ)
+    @GetMapping("/todos")
+    public String mostrarTodosNinjas(){
+        return "Mostrar ninjas";
+    }
+
+    //Alterar dados dos ninjas (UPDATE)
+    @PutMapping("/alterarID")
+    public String alterarNinja(){
+        return "Altera ninja por id";
+    }
+
+    //Deletar Ninja (DELETE)
+    @DeleteMapping("/deletaID")
+    public String deletarNinja(){
+        return "Deleta ninja";
     }
 
 }
