c61da5e0b249598d0891bbe75055f11de25960df
Anderson Junior <136769269+AndersonJunior16@users.noreply.github.com>
2025-03-07 01:13:26 -0300

    feat: Adiciona MissoesService e MissoesController, também com seu respectivo CRUD

--- src/main/java/dev/Juninho16/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/dev/Juninho16/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,33 +1,48 @@
 package dev.Juninho16.CadastroDeNinjas.Missoes;
 
+import dev.Juninho16.CadastroDeNinjas.Ninjas.NinjaService;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("missoes")
 public class MissoesController {
 
+    private MissoesService missoesService;
+
+    public MissoesController(MissoesService missoesService) {
+        this.missoesService = missoesService;
+    }
+
     //GET -- Mandar requisição parar listar missões
     @GetMapping("/listar")
-    public String listarMissao(){
-        return "Listar missões";
+    public List<MissoesModel> listarMissao(){
+        return missoesService.listarMissoes();
+    }
+
+    //GET -- Mandar requisição para listar missão por id
+    @GetMapping("/listar/{id}")
+    public MissoesModel missao(@PathVariable Long id){
+        return missoesService.listarmissaoId(id);
     }
 
     //POST -- Mandar requisição parar criar missões
     @PostMapping("/criar")
-    public String criarMissao(){
-        return "Cria missão";
+    public MissoesModel criarMissao(@RequestBody MissoesModel missao){
+        return missoesService.criarMissao(missao);
     }
 
     //PUT -- Mandar requisição parar alterar missões
     @PutMapping("/alterar")
-    public String aletrarMissao(){
-        return "Altera missão";
+    public MissoesModel aletrarMissao(@PathVariable Long id, @RequestBody MissoesModel missao){
+        return missoesService.atualizarMissao(id, missao);
     }
 
     //DELETE -- Mandar requisição parar deletar missões
-    @DeleteMapping("/deletar")
-    public String deletaMissao(){
-        return "Deleta missão";
+    @DeleteMapping("/deletar/{id}")
+    public void deletaMissao(@PathVariable Long id){
+        missoesService.deletarMissao(id);
     }
 
 
